2276fa0b4f0b18a95849e85b36788c656f574187
cschsz <c@sch-sz.de>
2018-05-09 18:27:56 +0200

    add timechecking

--- README.md
+++ README.md
@@ -2,3 +2,11 @@
 In the first step I want to make an eye friendly page in which Rooms and Temperatures are displayed. 
 The incoming string looks like this:
 $1;1; ;25,9; ;23,8;24,4;24,8;25,4;23,6;23,2;54; ;61;61;60;58;63;63; ; ; ; ; ;0
+
+## Preparation
+* sudo apt-get install python-pip
+* pip install schedule
+
+## Installation
+* sudo chmod +x serialmon_01-py
+* ...
--- serialmon_01.py
+++ serialmon_01.py
@@ -1,24 +1,32 @@
 #!/usr/bin/python3 -u
+# -*- coding: utf-8 -*-
 
+import schedule
 import serial
+import time
 import sys
 import os
 
 port = '/dev/ttyUSB0'     # serial port of USB-WDE1
 
+#----------------------------[once_a_day]
+def once_a_day():
+    print ("once_a_day")
+    return
+
 #----------------------------[analyze]
 def analyze(line):
-    line = line.strip(b"$1;1;;")
+    line = line.strip("$1;1;;")
     print (line)
-    line = (line.split(b";"))   # Data is a bytes object !!! Therefore b
-    print (b"Obergesch " + line[0] + b" C    " + line[ 8] + b" %")
-    print (b"Halle     " + line[1] + b" C    " + line[ 9] + b" %")
-    print (b"Schlafzi  " + line[2] + b" C    " + line[10] + b" %")
-    print (b"Toilette  " + line[3] + b" C    " + line[11] + b" %")
-    print (b"Badezi    " + line[4] + b" C    " + line[12] + b" %")
-    print (b"Kueche    " + line[5] + b" C    " + line[13] + b" %")
-    print (b"Heizung   " + line[6] + b" C    " + line[14] + b" %")
-    print (b"Buero     " + line[7] + b" C    " + line[15] + b" %")
+    line = (line.split(";"))
+    print ("Obergesch " + line[0] + "°C    " + line[ 8] + " %")
+    print ("Halle     " + line[1] + "°C    " + line[ 9] + " %")
+    print ("Schlafzi  " + line[2] + "°C    " + line[10] + " %")
+    print ("Toilette  " + line[3] + "°C    " + line[11] + " %")
+    print ("Badezi    " + line[4] + "°C    " + line[12] + " %")
+    print ("Kueche    " + line[5] + "°C    " + line[13] + " %")
+    print ("Heizung   " + line[6] + "°C    " + line[14] + " %")
+    print ("Buero     " + line[7] + "°C    " + line[15] + " %")
     return
 
 #----------------------------[main]
@@ -29,11 +37,15 @@ def main():
         sys.exit(1)
     print ("connected: " + port)
 
+    schedule.every().day.at("08:00").do(once_a_day)
+
     while True:
+        schedule.run_pending()      # check clock
         line = ser.readline()       # read line from WDE1
-        analyze(line)
-     
+        #line = "$1;1;;30;31;32;33;34;35;36;37;50;51;52;53;54;55;56;57"
+        analyze(line)               # analyze
+        #time.sleep(5)
+
 #----------------------------[]     
 if __name__=='__main__':
-    #analyze("$1;1;;30;31;32;33;34;35;36;37;50;51;52;53;54;55;56;57")
     main()

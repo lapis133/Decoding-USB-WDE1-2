f6c2c1ad23f3ee10139bdb0b4af4fe5c32bf78cc
lapis133 <pm-stein@gmx.de>
2018-05-27 19:38:28 +0200

    update 21.05.2018

--- serialmon_01.py
+++ serialmon_01.py
@@ -1,42 +1,78 @@
 #!/usr/bin/python3 -u
-
-import serial,sys,os
+#!-*- coding: utf-8 -*-
+# 21.05.2018
+import time,smtplib,serial,sys,os
 
 # serial port of USB-WDE1
 
 port = '/dev/ttyUSB0'
 
-# MAIN
-
-def main():
-  # open serial line
-  ser = serial.Serial(port,9600)
+#------------------------------------------[serial_init]
+def serial_init():
+  global ser
+  global line
+  ser = serial.Serial(port,9600)       # open serial line
   if not ser.isOpen():
     print ("Unable to open serial port %s") % port
     sys.exit(1)
   print ("connected: " + port)
-
+  return
+#--------------------------------------------[ End ]
   while True:
-   
-                             # read line from WDE1
-   line = ser.readline()
-   #print (line)
+                    
+   line = ser.readline()      # read line from WDE1
+   print (line)   
    line = line.strip(b"$1;1;;")
-   print (line)
-   #ln1 = line[0:6]           # Slice
-   #ln1 = line.partition(b";")
+#   print (line)
+#   values = repr (line)   
+   values = (line.split(b";"))  # Data is a bytes object !!! Therefore b
+  return
+#------------------------------------------[analyze(line)]
+def analyse(line):
+   global values
+
+   line = line.strip(b"$1;1;;")
+#   print (line)
+#   values = repr (line)   
+   values = (line.split(b";"))  # Data is a bytes object !!! Therefore b
    
-   line = (line.split(b";"))  # Data is a bytes object !!! Therefore b
-   #for i in range(0,8):      # Results in a Tuple
-      #print (line[i])
-   print (b"Obergesch " + line[0] + b" C  " + line[8] + b" %")
-   print (b"Halle     " + line[1] + b" C  " + line[9] + b" %")
-   print (b"Schlafzi  " + line[2] + b" C  " + line[10] + b" %")
-   print (b"Toilette  " + line[3] + b" C  " + line[11] + b" %")
-   print (b"Badezi    " + line[4] + b" C  " + line[12] + b" %")
-   print (b"Kueche    " + line[5] + b" C  " + line[13] + b" %")
-   print (b"Heizung   " + line[6] + b" C  " + line[14] + b" %")
-   print (b"Buero     " + line[7] + b" C  " + line[15] + b" %")
+   print (b"Obergesch " + values[0] + b" C  " + values[8] + b" %")
+   print (b"Halle     " + values[1] + b" C  " + values[9] + b" %")
+   print (b"Schlafzi  " + values[2] + b" C  " + values[10] + b" %")
+   print (b"Toilette  " + values[3] + b" C  " + values[11] + b" %")
+   print (b"Badezi    " + values[4] + b" C  " + values[12] + b" %")
+   print (b"Kueche    " + values[5] + b" C  " + values[13] + b" %")
+   print (b"Heizung   " + values[6] + b" C  " + values[14] + b" %")
+   print (b"Buero     " + values[7] + b" C  " + values[15] + b" %")
+   print ()
+   return
+#--------------------------------------------[ End ]
+
+#--------------------------------------------[ Show Time ]
+def showtime():
+  global lt
+
+  lt = time.localtime()   # aktuelle zeit als Tupel
+  stunde, minute, sekunde = lt[3:6]
+  print(time.strftime("%d.%m.%Y Time: %H:%M:%S", lt))
+  print()
+  return
+#--------------------------------------------[ End ]
+  
+# MAIN
+
+def main():
+
+    serial_init()
+
+    while True:
+                               
+     line = ser.readline()   # read line from WDE1
+     #print (line)   
+#     print(b"\033[H\033]J")
+     showtime()
+     analyse(line)
+   
    
 if __name__=='__main__':
 

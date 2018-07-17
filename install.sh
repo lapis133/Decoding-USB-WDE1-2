a33f359cd99b35408fc2e6f0ccb2af27d119d547
Max Truxa <dev@maxtruxa.com>
2018-07-17 11:14:49 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-sudo cp -n serialmon_01.ini /usr/local/etc 
+if [ ! -f /usr/local/etc/serialmon_01.ini ]; then
+    sudo cp -n serialmon_01.ini /usr/local/etc
+fi
 sudo cp serialmon_01.py /usr/local/bin
 sudo chmod +x /usr/local/bin/serialmon_01.py
 sudo rm /var/log/serialmon_info.log

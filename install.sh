0a4a3fee0f41d5c879ae3db33d5dcc3c0dd4df39
lapis133 <38972149+lapis133@users.noreply.github.com>
2018-07-26 13:59:35 +0200

    Hash out .pem and info.log

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 sudo cp -n serialmon_01.ini /usr/local/etc
-sudo cp serialmon_01.pem /usr/local/etc
+#sudo cp serialmon_01.pem /usr/local/etc
 sudo mkdir /usr/local/bin/serialmon
 sudo cp *.py /usr/local/bin/serialmon
 sudo chmod +x /usr/local/bin/serialmon/serialmon_01.py
 sudo chmod +x /usr/local/bin/serialmon/serialmon_sensor.py
-sudo rm /var/log/serialmon_info.log
+#sudo rm /var/log/serialmon_info.log
